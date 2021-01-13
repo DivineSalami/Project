529ac9a2b9f59429305323cbf90b968749ba653d
DivineSalami <72221858+DivineSalami@users.noreply.github.com>
2021-01-13 12:27:06 -0600

    Add files via upload

---
+++ Project/Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project
+//
+//  Created by P.M. Student on 1/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, General Ken!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
